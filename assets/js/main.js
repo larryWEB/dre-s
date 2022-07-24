82d4a03d9003f98a7c928c95a59dfdea976453ce
LarryWEB <103619061+larryWEB@users.noreply.github.com>
2022-07-24 01:49:57 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
-let mybutton = document.getElementById("#scroll-top");
-
-window.onscroll = function() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-};
-
-mybutton.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
-
 
 
 //function for sliding images
